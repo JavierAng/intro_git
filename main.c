7072bef24cf93e4dce3a1b94aa94ccf58958119f
Javier Anguiano <javier.anguiano4523@alumnos.udg.mx>
2022-05-31 14:07:56 -0500

    Itera nombres

--- main.c
+++ main.c
@@ -6,5 +6,12 @@ int main()
     printf("Hola bro\n");
     printf("Ingresa tu nombre: ");
     scanf("%s",&nombre);
+
+    size_t i;
+    for (i = 0; i < 10; i++)
+    {
+        printf("Nombre %i: %s \n",i,nombre);
+    }
+    
     return 0;
 }
